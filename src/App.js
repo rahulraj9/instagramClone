ab0ab0ae8dfad3ecc4d32e7bf25fb2202a17b9e6
RahulRaj <jokingboyrahul@gmail.com>
2021-04-02 13:19:47 +0530

    [Rahul] Rendering Data from firebase for posts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Post from './Post'
-import React ,{useState} from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { db } from './firebase'
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "Elon Musk",
-      caption: " wow",
-      imageurl: "https://cdn.vox-cdn.com/thumbor/05Fm-f6FL70DdyFKmh6jd7sz0TI=/0x0:4000x2840/1200x800/filters:focal(2335x1426:2975x2066)/cdn.vox-cdn.com/uploads/chorus_image/image/69001757/1229901940.0.jpg"
-    },
-    {
-      username: "Mark zuckerberg",
-      caption: " wow",
-      imageurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8UdZriwrQVP9YaUwT96ScZTmMYYhfNRi1Gw&usqp=CAU"
-    }
-  ])
+  const [posts, setPosts] = useState([])
+
+  //useEffect Run a peice of code on specif condition
+
+
+  useEffect(() => {
+
+    db.collection('posts').onSnapshot(snapshot => {
+
+      setPosts(snapshot.docs.map(doc => doc.data()))
+      console.log(setPosts(snapshot.docs.map(doc => doc.data())))
+    })
+  }, [])
+
   return (
     <div className="App">
       <div className="app_header">
         <img
           className="app_headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt=""></img>
+          alt=" "></img>
       </div>
 
       {
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase'
+
+
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBsVGpak0-RkRg5K0fLVjUdvpxlGBf6GXc",
     authDomain: "instagram-clone-134c0.firebaseapp.com",
@@ -8,7 +10,10 @@ const firebaseApp = firebase.initializeApp({
     appId: "1:794068919509:web:04d6a9d7c6627ff6802e5c",
     measurementId: "G-GM7NE3KW6K"
 });
+
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
+
+
 export { db, auth, storage }
